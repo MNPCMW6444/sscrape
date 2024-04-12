f4faa768dc8f857b25122f425ebbdae82e3328a0
GitHub Action <michael+cicd@offisito.com>
2024-04-12 10:30:55 +0300

    extension init fixes

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     },
     rollupOptions: {
       input: {
+        index: 'index.html',
         background: 'src/background/index.ts',
         contentScript: 'src/contentScript/index.ts',
         popup: 'src/popup/index.tsx',
